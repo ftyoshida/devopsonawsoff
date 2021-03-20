441e44caa210ce43100813773be0dd8a442d60e2
fabio toshio <ftyoshida@gmail.com>
2021-03-20 09:35:17 -0300

    change message - manual approve

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello 337920! com passo manual')
+  res.send('Hello 337920! com passo manual com aprovacao')
 })
 
 app.listen(port, () => {
